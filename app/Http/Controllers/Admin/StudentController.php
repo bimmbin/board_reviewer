78f220bec120d345d746501aa7911bcbe502f2ce
bimmbin <bimmbin.gomez@gmail.com>
2023-12-30 22:15:32 +0800

    feat: student account creation, search, and student lists pagination

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Builder;
 
 class StudentController extends Controller
@@ -31,21 +32,64 @@ class StudentController extends Controller
    */
   public function store(Request $request)
   {
-    //
+    $request->validate([
+      'student_id' => 'required|integer|unique:' . User::class,
+      'first_name' => 'required|regex:/^[a-zA-Z]+$/u|max:255',
+      'last_name' => 'required|regex:/^[a-zA-Z]+$/u|max:255',
+      'middle_name' => 'required|regex:/^[a-zA-Z]+$/u|max:255',
+    ]);
+
+    $student = User::firstOrNew(
+      [
+        'student_id' => $request->student_id
+      ],
+      [
+        'major_id' => 1,
+        'user_role' => 'student',
+        'first_name' => $request->first_name,
+        'last_name' => $request->last_name,
+        'middle_name' => $request->middle_name,
+        'username' => $request->last_name . $request->student_id,
+        'password' => Hash::make($request->student_id)
+      ]
+    );
+    if (!$student->exists) {
+      $student->save();
+    };
+
+    return redirect()->back();
   }
 
   /**
    * Display the specified resource.
    */
-  public function show(string $id)
+  public function show(Request $request, string $id)
   {
+
     $students = User::whereHas('major', function (Builder $query) use ($id) {
       $query->where('id', $id);
-    })->get();
-
+    })->when($request->input('search'), function ($query, $search) {
+      $query->where('first_name', 'like', "%{$search}%")
+        ->orWhere('last_name', 'like', "%{$search}%")
+        ->orWhere('middle_name', 'like', "%{$search}%")
+        ->orWhere('student_id', 'like', "%{$search}%");
+    })->latest()
+      ->paginate(10)
+      ->withQueryString()
+      ->through(function ($item) {
+        return [
+          'id' => $item->id,
+          'first_name' => $item->first_name,
+          'last_name' => $item->last_name,
+          'middle_name' => $item->middle_name,
+          'student_id' => $item->student_id,
+        ];
+      });
 
     return Inertia::render('Admin/Students', [
-      'students' => $students
+      'students' => $students,
+      'filters' => $request->only('search'),
+      'major_id' => $id,
     ]);
   }
 
--- resources/js/Pages/Admin/Students.vue
+++ resources/js/Pages/Admin/Students.vue
@@ -7,20 +7,62 @@ export default {
 </script>
 
 <script setup>
-import TableTemplate from "@/Components/TableTemplate.vue";
+import { ref, watch } from "vue";
+import { router, Link } from "@inertiajs/vue3";
+import { Collapse } from "vue-collapsed";
 
-const { students } = defineProps({
+import Pagination from "@/Components/Pagination.vue";
+import CreateStudent from "@/Components/CreateStudent.vue";
+
+const { students, major_id, filters } = defineProps({
     students: Object,
+    filters: Object,
+    major_id: String,
+});
+
+let show_details = ref(false);
+
+const search = ref(filters.search);
+
+watch(search, (value) => {
+    router.get(
+        ("/students/", major_id),
+        { search: value },
+        {
+            preserveState: true,
+            replace: true,
+        }
+    );
 });
 </script>
 
 <template>
-    <h1
-        class="text-3xl font-semibold mb-5 max-md:mt-20 text-blue-800 max-md:mb-2"
-    >
-        Students
-    </h1>
-    <div class="w-full pb-10 overflow-x-auto">
+    <div class="w-full flex items-center justify-between">
+        <div class="flex items-center gap-10" :class="{ 'opacity-20' : show_details }">
+            <Link :href="route('students.show', major_id)"
+                class="text-3xl font-semibold max-md:mt-20 text-blue-800 max-md:mb-2"
+            >
+                Students
+            </Link>
+            <input
+                v-model="search"
+                type="text"
+                placeholder="Search...."
+                class="rounded-md border border-blue-300 w-[25rem]"
+            />
+        </div>
+        <div @click="show_details = !show_details" class="bg-blue-500 text-white px-5 py-3 rounded-md select-none cursor-pointer">
+            <span v-if="!show_details">Create new</span>
+            <span v-else>Close</span>
+        </div>
+    </div>
+
+    <!-- Create student form -->
+    <Collapse :when="show_details">
+        <CreateStudent @close_emit="show_details = !show_details"/>
+    </Collapse>
+
+    <div class="w-full pb-10 overflow-x-auto mt-5 " :class="{ 'opacity-20' : show_details }">
         <table
             id="dataTable"
             class="table-auto text-center w-[1220px] max-lg:w-[1080px] max-sm:w-[900px] text-lg xl:w-full"
@@ -30,15 +72,18 @@ const { students } = defineProps({
                 <tr
                     class="space-y-3 text-sm md:text-base border border-blue-500 lg:text-lg text-start text-btn bg-blue-500 text-white"
                 >
-                    <th class="text-left pl-5 py-5 font-semibold">Student ID</th>
+                    <th class="text-left pl-5 py-5 font-semibold">
+                        Student ID
+                    </th>
                     <th class="text-left py-5 font-semibold">Last Name</th>
                     <th class="text-left py-5 font-semibold">First Name</th>
                     <th class="text-left py-5 font-semibold">Middle Name</th>
                 </tr>
             </thead>
 
-            <tbody class="text-left">
-                <tr v-for="student in students"
+            <tbody class="text-left capitalize">
+                <tr
+                    v-for="student in students.data"
                     class="border border-blue-300 text-sm md:text-base lg:text-lg font-regular"
                 >
                     <td class="pl-5 py-3">{{ student.student_id }}</td>
@@ -48,5 +93,6 @@ const { students } = defineProps({
                 </tr>
             </tbody>
         </table>
+        <Pagination :links="students.links" />
     </div>
 </template>
